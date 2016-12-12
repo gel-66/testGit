d4e88b2aa2fe04fd545b462ced68083e7651b3cd
Gary <enlegan@qq.com>
2016-12-12 23:50:14 +0800

    git test commit

--- testGit/src/com/gary/git/test.java
+++ testGit/src/com/gary/git/test.java
@@ -3,8 +3,10 @@ package com.gary.git;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("aaaaaaaaaa");
+		
+		
 	}
 
 }
